2d03999fb3180ec544d4f59a9d4bb5b784f9a1b7
Charankrishcherry <72595246+Charankrishcherry@users.noreply.github.com>
2024-10-24 14:55:01 +0530

    Update outputs.tf

--- modules/azure-storage-account/outputs.tf
+++ modules/azure-storage-account/outputs.tf
@@ -1,4 +1,19 @@
+# modules/storage/outputs.tf
+ 
+output "storage_account_id" {
+  value = azurerm_storage_account.storage.id
+}
+ 
 output "storage_account_name" {
-  value       = azurerm_storage_account.storage_account.name
-  description = "Name of the deployed storage account."
+  value = azurerm_storage_account.storage.name
+}
+ 
+output "storage_account_primary_access_key" {
+  value     = azurerm_storage_account.storage.primary_access_key
+  sensitive = true
+}
+ 
+output "primary_connection_string" {
+  value     = azurerm_storage_account.storage.primary_connection_string
+  sensitive = true
 }
