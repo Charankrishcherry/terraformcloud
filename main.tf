2dd1db0ff566d36ae63692220634573ac4e1f14e
Charankrishcherry <72595246+Charankrishcherry@users.noreply.github.com>
2024-10-24 12:38:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-module "storage" {  # You can name the module instance whatever you like.
-  source                = "./modules/azure-storage-account"
-  storage_account_name  = "mystorageaccountuniquename" # Make sure this is unique
-  resource_group_name  = "your-resource-group"
-  location              = "West US 2" # Or your preferred region
-  account_tier         = "Standard"
-  account_replication_type = "LRS"
+module "storage" {
+  source                     = "./modules/azure-storage-account"
+  storage_account_name       = "storageacct${random_id.suffix.hex}"
+  resource_group_name         = var.resource_group_name
+  location                   = var.location
+  account_tier                = var.storage_account_tier
+  account_replication_type    = var.account_replication_type
   tags = {
-    owner       = "your-name"
-    environment = "production"
+    environment = var.environment_tag
+    created_by  = "Terraform"
   }
 }
