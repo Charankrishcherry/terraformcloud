19a0aad610c158eaa9174af30ddde256a75daf22
Charankrishcherry <72595246+Charankrishcherry@users.noreply.github.com>
2024-10-24 14:54:36 +0530

    Update main.tf

--- modules/azure-storage-account/main.tf
+++ modules/azure-storage-account/main.tf
@@ -1,23 +1,8 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0" # Or your desired version
-    }
-  }
-}
-
-provider "azurerm" { # Add the provider block
-  features {}  # This empty block is now explicitly included
-}
-
-resource "azurerm_storage_account" "storage_account" {
+resource "azurerm_storage_account" "storage" {
   name                     = var.storage_account_name
   resource_group_name      = var.resource_group_name
   location                 = var.location
-  account_tier             = var.account_tier
-  account_replication_type = var.account_replication_type
-  min_tls_version          = var.min_tls_version  # Important for security
-
-  tags = var.tags
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  min_tls_version           = "TLS1_2"
 }
