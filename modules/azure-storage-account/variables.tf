a15debee2bb0a5c038fbb6f2d90b05978c199119
Charankrishcherry <72595246+Charankrishcherry@users.noreply.github.com>
2024-10-24 14:55:22 +0530

    Update variables.tf

--- modules/azure-storage-account/variables.tf
+++ modules/azure-storage-account/variables.tf
@@ -1,39 +1,11 @@
-variable "storage_account_name" {
-  type        = string
-  description = "Unique storage account name."
-}
-
 variable "resource_group_name" {
-  type        = string
-  description = "Resource group name."
-}
-
-variable "location" {
-  type        = string
-  description = "Azure region."
-}
-
-variable "account_tier" {
-  type        = string
-  description = "Storage account tier."
-  default     = "Standard"
-}
-
-variable "account_replication_type" {
-  type        = string
-  description = "Replication type."
-  default     = "LRS"
-}
-
-variable "min_tls_version" {
   type = string
-  description = "Minimum TLS version. Recommended: TLS1_2 for security."
-  default = "TLS1_2"
 }
-
-
-variable "tags" {
-  type        = map(string)
-  description = "Tags for the storage account."
-  default     = {}
+ 
+variable "location" {
+  type = string
+}
+ 
+variable "storage_account_name" {
+  type = string
 }
