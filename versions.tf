b4395e93f0d316b1b155cd8b179b8370dba1bcba
Charankrishcherry <72595246+Charankrishcherry@users.noreply.github.com>
2024-10-24 12:39:48 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,9 +1,10 @@
 terraform {
-  required_version = ">= 1.2.0"  # Or your preferred version
+  required_version = ">= 1.2.0" # Or your specific version
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"  # Or your preferred version
+      version = "~> 3.0"  # Or your specific version constraint
     }
   }
 }
