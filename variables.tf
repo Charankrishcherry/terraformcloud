8c978122e6bf91cae4a685bd6fed7fc7e7bb6e21
Charankrishcherry <72595246+Charankrishcherry@users.noreply.github.com>
2024-10-24 14:57:13 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,30 +1,14 @@
 variable "resource_group_name" {
-  type        = string
-  description = "The name of the existing resource group."
-}
-
-variable "location" {
-  type        = string
-  description = "The Azure region for deployment."
-}
-
-variable "storage_account_tier" {  # Default can be overridden in tf cloud
-  type        = string
-  description = "The storage account tier (Standard or Premium)."
-  default     = "Standard"
-}
-
-variable "account_replication_type" {
-  type        = string
-  description = "Replication type (LRS, GRS, etc.)."
-  default     = "LRS" # Example
-}
-
-
-
-variable "environment_tag" {
   type = string
-  default = "development"
-  description = "Tag for the environment."
-
+  default = "Oxxo_Test"
+}
+ 
+variable "location" {
+  type = string
+  default = "eastus"
+}
+ 
+variable "storage_account_name" {
+  type = string
+  default = "oxxotestaccountsa"
 }
