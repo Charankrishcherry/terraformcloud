4d8660fdd6672c997620ac5cb48c10fd0f9a19d7
Charankrishcherry <72595246+Charankrishcherry@users.noreply.github.com>
2024-10-24 14:55:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,6 @@
 module "storage" {
-  source                     = "./modules/azure-storage-account"
-  storage_account_name       = "Simplename"
-  resource_group_name         = var.resource_group_name
-  location                   = var.location
-  account_tier                = var.storage_account_tier
-  account_replication_type    = var.account_replication_type
-  tags = {
-    environment = var.environment_tag
-    created_by  = "Terraform"
-  }
+  source               = "./modules/storage"
+  resource_group_name  = var.resource_group_name
+  location             = var.location
+  storage_account_name = var.storage_account_name
 }
